24e93c5201f0f11eb1dc1338c73986b22aa844db
Erion <24631-Erion@users.noreply.gitgud.io>
2023-04-26 16:29:12 +0000

    Update spniBehaviour.js

--- js/spniBehaviour.js
+++ js/spniBehaviour.js
@@ -1350,7 +1350,7 @@ function expandPlayerVariable(split_fn, args, player, self, target, bindings) {
     case 'wearing':
         {
             var types = [], positions = [], names = [];
-            args.split('|').forEach(function(keyword) {
+            (args ? args.split('|') : []).forEach(function(keyword) {
                 if ([IMPORTANT_ARTICLE, MAJOR_ARTICLE, MINOR_ARTICLE, EXTRA_ARTICLE].indexOf(keyword) >= 0) {
                     types.push(keyword);
                 } else if ([UPPER_ARTICLE, LOWER_ARTICLE, FULL_ARTICLE, OTHER_ARTICLE,
@@ -1773,7 +1773,7 @@ function evalOperator (val, op, cmpVal) {
  * the current state marker.
  ************************************************************/
 function checkMarker(predicate, self, target, currentOnly) {
-    var match = predicate.match(/^([\w\-]+)(\*?)(\s*(\<\=|\>\=|\<|\>|\=\=|!\=|\=|\@)?\s*(.+))?\s*$/);
+    var match = predicate.match(/^([\w\-\+]+)(\*?)(\s*(\<\=|\>\=|\<|\>|\=\=|!\=|\=|\@)?\s*(.+))?\s*$/);
     
     var name;
     var perTarget;
