e6a424580cbe89c6ec2020ce219129ac1a2e9c41
Kobrad <doctorakimbo@gmail.com>
2023-11-13 05:04:32 -0500

    Turn off Nuzlocke Mode

--- js/spniCore.js
+++ js/spniCore.js
@@ -605,6 +605,11 @@ function restartGame () {
     resetPlayers();
     currentRound = -1;
 
+    var ids = save.selectedClothingIDs();
+    for (var id of ids) {
+        PLAYER_CLOTHING_OPTIONS[id].removed = false;
+    }
+
     /* enable table opacity */
     tableOpacity = 1;
     $gameTable.css({opacity:1});
